62cf8d3dcc68c43ed13a4b2c5ed70d0c4c9b90f6
Andy Ball <andy_ball@32097.gnm.int>
2018-07-25 15:10:49 +1000

    Updates

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ import { election } from './modules/election'
 
 var key = '1wZXnPwxMfwjNvIYTm2PLbKWooyLLJifHImD71P8KsM8' 
 
-xr.get('https://interactive.guim.co.uk/docsdata/' + key + '.json').then((resp) => {
+xr.get('https://interactive.guim.co.uk/docsdata/' + key + '.json?t=' + new Date().getTime()).then((resp) => {
 
 	let googledoc = resp.data.sheets;
 
